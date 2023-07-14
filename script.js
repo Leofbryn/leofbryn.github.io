075236ef6d979208fa4fb275a42a3652d8ae942b
leofbryn <tariganfml@gmail.com>
2023-07-15 00:10:57 +0700

    first commit

---
+++ script.js
@@ -0,0 +1,8 @@
+window.addEventListener("load", function () {
+  setTimeout(function open(event) {
+    document.querySelector(".popup").style.display = "block";
+  }, 2000);
+});
+document.querySelector("#close").addEventListener("click", function () {
+  document.querySelector(".popup-start").style.display = "none";
+});
